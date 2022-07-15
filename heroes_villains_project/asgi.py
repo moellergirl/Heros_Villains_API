b7f2fb126a33537e94f79fe4e57ffb0e8e2d0ce7
Hot Mama <hotmama@Hots-MacBook-Pro.local>
2022-07-15 13:15:53 -0700

    re-created local.settings.py + updated settings.py

--- heroes_villains_project/asgi.py
+++ heroes_villains_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for heros_villains_project project.
+ASGI config for heroes_villains_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heros_villains_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heroes_villains_project.settings')
 
 application = get_asgi_application()
--- heroes_villains_project/urls.py
+++ heroes_villains_project/urls.py
@@ -1,4 +1,4 @@
-"""heros_villains_project URL Configuration
+"""heroes_villains_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
