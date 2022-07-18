d578a964d30336195687c8313ab25f8c3b8d177f
Hot Mama <hotmama@Hots-MBP.hsd1.or.comcast.net>
2022-07-18 16:37:42 -0700

    updated project urls+ tired to figure out why the database does not exsisit now

--- supers/urls.py
+++ supers/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from. import views
 
 urlpatterns=[
-    path ('', views.super_list)
+    path ('', views.Super_list,)
 ]
--- supers/views.py
+++ supers/views.py
@@ -1,7 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from. serializers import SuperSerializer
+from.models import Super
+
 
 @api_view (['GET'])
-def super_list (request):
-    return Response ('Howdy')
+def Super_list (request):
+    supers=Super.objects.all ()
+    serializer= SuperSerializer (supers, many=True)
+    return Response (serializer.data)
 
