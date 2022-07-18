f7ae710c92c4bf49ea760630e68ee1a094cddc8b
Hot Mama <hotmama@Hots-MBP.hsd1.or.comcast.net>
2022-07-18 10:51:53 -0700

    Modified serializers in super_types views

--- super_types/views.py
+++ super_types/views.py
@@ -1,9 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from.serializers import SuperTypeSerializer
+from.models import SuperType
 
 
 @api_view(['GET'])
 def SuperType_list(reguest):
-    return Response ('Hey! Yo!')
+    super_types= SuperType.objects.all()
+    serializer=SuperTypeSerializer(super_types,many=True)
+
+    return Response (serializer.data)
 
