803114cd19f4bc2a1dcb774fd1310eca96fa98a9
Hot Mama <hotmama@Hots-MBP.hsd1.or.comcast.net>
2022-07-19 08:48:37 -0700

    Registered Supers with Admin site + updated SuperSerializer

--- supers/serializers.py
+++ supers/serializers.py
@@ -3,4 +3,5 @@ from.models import Super
 
 class SuperSerializer (serializers.ModelSerializer):
     class Meta:
-        fields=['id','name','alter_ego','primary_ability','secondary_ability', 'cathphrase','super_type']
+        model=Super
+        fields=['id','name','alter_ego','primary_ability','secondary_ability', 'catchphrase','super_type']
