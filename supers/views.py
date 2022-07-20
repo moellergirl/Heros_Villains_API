5e18796aca3fda97f0d8c6dcd64d95b67b9318db
Hot Mama <hotmama@Hots-MBP.hsd1.or.comcast.net>
2022-07-19 17:42:43 -0700

    Modified veiws multiple times to try to get psotmasn to work after I put in params it seem to break

--- supers/views.py
+++ supers/views.py
@@ -7,14 +7,20 @@ from.models import Super
 
 
 @api_view (['GET','POST'])
+
 def Super_list (request):
-    if request.method== 'GET':
-        supers=Super.objects.all ()
-        super_types_param= request.query_params.get ('super_types')
-        sort_param = request.query_params.get ('sort')
-        print (super_types_param)
-        print (sort_param)
-        serializer= SuperSerializer (supers, many=True)
+    if request.method == 'GET':
+        super_type_param=request.query_params.get ('super_types')
+        sort_param=request.query_params.get('sort')
+        print(super_type_param)
+        print(sort_param)
+        supers=Super.objects.all()
+    if super_type_param:
+        supers = supers.filter(super_type__type=super_type_param)   
+
+    if sort_param:
+        supers= supers.order_by(sort_param)
+        serializer=SuperSerializer(supers, many=True)
         return Response (serializer.data)
 
     elif request.method == 'POST':
@@ -23,17 +29,19 @@ def Super_list (request):
         serializer.save()
         return Response (serializer.data,status= status.HTTP_201_CREATED)
 
-@api_view (['GET'])
+@api_view (['GET','PUT','DELETE'])
 def supers_details(request,pk):
     supers=get_object_or_404(Super, pk=pk)
     if request.medtod == 'GET':
         serializer=SuperSerializer (supers, pk=pk)
         return Response(serializer.data)
+
     elif request.method == 'PUT':  
       serializer = SuperSerializer(supers, data=request.data)
       serializer.is_valid(raise_exception=True)
       serializer.save()
       return Response(serializer.data)
+      
     elif request.method == 'DELETE':  
         supers.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)    
